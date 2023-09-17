2a7106ef98305f444b484c4db7c9e69add49aa1d
Nightliuguoxing <35206019+Nightliuguoxing@users.noreply.github.com>
2023-09-17 14:12:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+pip install torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
+pip install torch_geometric
 unzip -d ./data/Weibo ./data/Weibo/weibotree.txt.zip
+mkdir ./data/Weibograph
 python ./Process/getWeibograph.py
 python ./model/Weibo/BiGCN_Weibo.py 100
