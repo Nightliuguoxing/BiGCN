5e00896fc1f76e69c4f52b2fa25fe6b7a3a322d5
Nightliuguoxing <2050180797@qq.com>
2023-10-08 11:38:23 +0800

    LGX

--- Process/process.py
+++ Process/process.py
@@ -5,19 +5,6 @@ cwd=os.getcwd()
 
 ################################### load tree#####################################
 def loadTree(dataname):
-    if 'Twitter' in dataname:
-        treePath = os.path.join(cwd,'data/'+dataname+'/data.TD_RvNN.vol_5000.txt')
-        print("reading twitter tree")
-        treeDic = {}
-        for line in open(treePath):
-            line = line.rstrip()
-            eid, indexP, indexC = line.split('\t')[0], line.split('\t')[1], int(line.split('\t')[2])
-            max_degree, maxL, Vec = int(line.split('\t')[3]), int(line.split('\t')[4]), line.split('\t')[5]
-            if not treeDic.__contains__(eid):
-                treeDic[eid] = {}
-            treeDic[eid][indexC] = {'parent': indexP, 'max_degree': max_degree, 'maxL': maxL, 'vec': Vec}
-        print('tree no:', len(treeDic))
-
     if dataname == "Weibo":
         treePath = os.path.join(cwd,'data/Weibo/weibotree.txt')
         print("reading Weibo tree")
